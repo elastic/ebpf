6948ed17c007f2cdd0a74cca7fbb7713ff5199dd
Michal Stanek <michal.stanek@elastic.co>
2021-03-23 22:43:12 +0100

    Fix style

--- non-GPL/HostIsolationMapsUtil/update_IPs.c
+++ non-GPL/HostIsolationMapsUtil/update_IPs.c
@@ -12,24 +12,27 @@
 
 #define warn(...) fprintf(stderr, __VA_ARGS__)
 
-static int libbpf_print_fn(enum libbpf_print_level level,
-        const char *format, va_list args)
+static int
+libbpf_print_fn(enum libbpf_print_level level,
+                const char *format,
+                va_list args)
 {
-    //if (level == LIBBPF_DEBUG && !env.verbose)
-//      return 0;
     return vfprintf(stderr, format, args);
 }
 
-int main(int argc, char **argv)
+int
+main(int argc,
+     char **argv)
 {
-    int map_IPs_fd;
+    int map_IPs_fd = -1;
     int rv = 0;
-    uint32_t key, val;
+    uint32_t key = 0, val = 0;
 
     libbpf_set_print(libbpf_print_fn);
 
     map_IPs_fd = bpf_obj_get("/sys/fs/bpf/tc/globals/allowed_IPs");
-    if (map_IPs_fd < 0) {
+    if (map_IPs_fd < 0)
+    {
         rv = -1;
         goto cleanup;
     }
@@ -37,13 +40,15 @@ int main(int argc, char **argv)
     val = 1;
     inet_pton(AF_INET, "172.67.197.155", &key);
     rv = bpf_map_update_elem(map_IPs_fd, &key, &val, 0);
-    if (rv) {
+    if (rv)
+    {
         warn("failed to add to IPs BPF map \n");
         goto cleanup;
     }
     inet_pton(AF_INET, "104.21.34.41", &key);
     rv = bpf_map_update_elem(map_IPs_fd, &key, &val, 0);
-    if (rv) {
+    if (rv)
+    {
         warn("failed to add to IPs BPF map \n");
         goto cleanup;
     }
@@ -52,9 +57,13 @@ int main(int argc, char **argv)
 
 cleanup:
     if (map_IPs_fd >= 0)
+    {
         close(map_IPs_fd);
-        if (rv)
-                warn("failure - run with sudo or make sure /sys/fs/bpf/tc/globals/allowed_IPs exists\n");
+    }
+    if (rv)
+    {
+        warn("failure - run with sudo or make sure /sys/fs/bpf/tc/globals/allowed_IPs exists\n");
+    }
 
     return rv;
 }
