18d6babff3d2842d92d1db13064064534291164d
Michal Stanek <michal.stanek@elastic.co>
2021-04-12 22:52:37 +0200

    Make Razor run eBPF build commands every time
    CustomCommands used for building eBPF subprojects will be run every time regardless of dependencies.
    This is ok because the CustomCommands mostly run 'make' which won't do anything if source files haven't changed.
    One exception is elftoolchain which has a bug which makes it rebuild every time bmake is run.
    However, elftoolchain sources won't ever be modified (there is only one .patch file for the Makefile) so we will skip building it if the build directory already exists.

--- contrib/build_libelf.sh
+++ contrib/build_libelf.sh
@@ -4,7 +4,7 @@ set -euv
 
 if [ ! -d contrib/elftoolchain/build ]; then
     mkdir -p contrib/elftoolchain/build_obj
-    export MAKEFLAGS="" && export MFLAGS="" && \
+    export MAKEFLAGS="" && export MFLAGS="" && MAKESYSPATH=/opt/endpoint-dev/dev/toolchain/share/mk \
         WITH_TESTS=no WITH_BUILD_TOOLS=no WITH_ADDITIONAL_DOCUMENTATION=no WITH_PE=no WITH_ISA=no   \
         MAKEOBJDIR=${PWD}/contrib/elftoolchain/build_obj \
         bmake -C contrib/elftoolchain
