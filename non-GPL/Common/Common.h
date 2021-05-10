ce3b269e6075c45be18f50304da6ec2afd22c247
Michal Stanek <michal.stanek@elastic.co>
2021-05-10 23:24:09 +0200

    Make macros for paths and add them to a new common header

---
+++ non-GPL/Common/Common.h
@@ -0,0 +1,8 @@
+
+#define EBPF_MAP_PARENT_DIRECTORY "/sys/fs/bpf/elastic"
+#define EBPF_MAP_DIRECTORY "/sys/fs/bpf/elastic/endpoint"
+#define EBPF_ALLOWED_IPS_MAP_NAME "allowed_IPs"
+#define EBPF_ALLOWED_IPS_MAP_PATH "/sys/fs/bpf/elastic/endpoint/allowed_IPs"
+#define EBPF_ALLOWED_PIDS_MAP_NAME "allowed_pids"
+#define EBPF_ALLOWED_PIDS_MAP_PATH "/sys/fs/bpf/elastic/endpoint/allowed_pids"
+
