58610314aa52aa23132d0f4be75637067e0a4cdb
Michal Stanek <michal.stanek@elastic.co>
2021-05-13 23:10:14 +0200

    Add Elastic 2.0 license disclaimers to non-GPL source files

--- non-GPL/HostIsolationMapsUtil/UpdateMaps.c
+++ non-GPL/HostIsolationMapsUtil/UpdateMaps.c
@@ -1,5 +1,10 @@
-// TODO:
-// LICENSE
+/*
+ * Copyright 2021 Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License 2.0;
+ * you may not use this file except in compliance with the Elastic License 2.0.
+ */
+
+
 //
 // Host Isolation - tool for updating map of allowed IPs and pids
 //
--- non-GPL/TcLoader/TcLoaderDemo.c
+++ non-GPL/TcLoader/TcLoaderDemo.c
@@ -1,5 +1,10 @@
-// TODO:
-// LICENSE
+/*
+ * Copyright 2021 Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License 2.0;
+ * you may not use this file except in compliance with the Elastic License 2.0.
+ */
+
+
 //
 // Host Isolation standalone demo
 // Loader for eBPF program #1 (replacement for 'tc filter add')
