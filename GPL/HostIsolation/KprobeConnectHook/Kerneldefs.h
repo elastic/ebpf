619002c3f82b97c3f493ac742b3293a12f04a13f
Michal Stanek <michal.stanek@elastic.co>
2021-05-13 22:58:06 +0200

    Add GPL license disclaimers to source files

--- GPL/HostIsolation/KprobeConnectHook/Kerneldefs.h
+++ GPL/HostIsolation/KprobeConnectHook/Kerneldefs.h
@@ -1,3 +1,22 @@
+/*
+ * Elastic eBPF
+ * Copyright 2021 Elasticsearch BV
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 typedef signed char __s8;
 
 typedef unsigned char __u8;
--- GPL/HostIsolation/KprobeConnectHook/KprobeConnectHook.bpf.c
+++ GPL/HostIsolation/KprobeConnectHook/KprobeConnectHook.bpf.c
@@ -1,6 +1,22 @@
-// TODO:
-// LICENSE
-//
+/*
+ * Elastic eBPF
+ * Copyright 2021 Elasticsearch BV
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 // Host Isolation - this eBPF program hooks into tcp_v4_connect kprobe and adds
 // entries to the IP allowlist if an allowed process tries to initiate a connection.
 
