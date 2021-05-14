b37296a8327cbd169cdc3a7f4320d96b98dcb217
Michal Stanek <michal.stanek@elastic.co>
2021-05-14 22:17:19 +0200

    Add workaround for old kernels rejecting kernel version parameter
    Old Linux kernels verify the kernel version parameter passed to the bpf() syscall. This parameter should match LINUX_VERSION_CODE in the running kernel. Add several methods of retrieving the value and retry loading if it failed.

--- non-GPL/HostIsolation/KprobeConnectHook/KprobeLoader.c
+++ non-GPL/HostIsolation/KprobeConnectHook/KprobeLoader.c
@@ -19,8 +19,108 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 
+#include <link.h>
+#include <sys/auxv.h>
 #include <Common.h>
 #include "KprobeLoader.h"
+
+#define MAX_LOAD_METHODS 2
+
+static unsigned int
+find_version_note(unsigned long base)
+{
+    ElfW(Ehdr) *ehdr;
+    unsigned int version_code = 0;
+    int i = 0;
+
+    if (!base)
+    {
+        ebpf_log("find_version_note error: NULL parameter\n");
+        goto out;
+    }
+
+    ehdr = (ElfW(Ehdr) *)base;
+
+    for (i = 0; i < ehdr->e_shnum; i++)
+    {
+        ElfW(Shdr) *shdr = (ElfW(Shdr) *)(
+            base + ehdr->e_shoff + (i * ehdr->e_shentsize)
+        );
+
+        if (shdr->sh_type == SHT_NOTE)
+        {
+            const char *ptr = (const char *)(base + shdr->sh_offset);
+            const char *end = ptr + shdr->sh_size;
+
+            while (ptr < end)
+            {
+                ElfW(Nhdr) *nhdr = (ElfW(Nhdr) *)ptr;
+                ptr += sizeof(*nhdr);
+
+                const char *name = ptr;
+                ptr += (nhdr->n_namesz + sizeof(ElfW(Word)) - 1) & -sizeof(ElfW(Word));
+
+                const char *desc = ptr;
+                ptr += (nhdr->n_descsz + sizeof(ElfW(Word)) - 1) & -sizeof(ElfW(Word));
+
+                if ((nhdr->n_namesz > 5 && !memcmp(name, "Linux", 5)) &&
+                    nhdr->n_descsz == 4 && !nhdr->n_type)
+                {
+                    version_code = *(uint32_t *)desc;
+                    goto out;
+                }
+            }
+        }
+    }
+
+out:
+    return version_code;
+}
+
+static unsigned int
+get_kernel_version(int method)
+{
+    unsigned int code = 0;
+    int rv = 0;
+    FILE *f = NULL;
+
+    switch (method)
+    {
+        case 0:
+        {
+            // Fetch LINUX_VERSION_CODE from the vDSO .note section.
+            // This always matches the running kernel, but is not supported on arm32.
+            unsigned long base = getauxval(AT_SYSINFO_EHDR);
+            // Check ELF magic value
+            if (base && !memcmp((void *)base, ELFMAG, 4))
+                code = find_version_note(base);
+        }
+        case 1:
+        {
+            // check if version.h exists
+            f = fopen("/usr/include/linux/version.h", "r");
+            if (!f)
+            {
+                goto out;
+            }
+            rv = fscanf(f, "#define LINUX_VERSION_CODE %d", &code);
+            if (rv != 1)
+            {
+                code = 0;
+                fclose(f);
+                goto out;
+            }
+            fclose(f);
+        }
+        default:
+        {
+            goto out;
+        }
+    }
+
+out:
+    return code;
+}
 
 struct bpf_object *
 ebpf_open_object_file(const char *file_path)
@@ -89,8 +189,25 @@ ebpf_load_and_attach_kprobe(struct bpf_object *obj,
     struct bpf_program *prog = NULL;
     struct bpf_link *link = NULL;
     int prog_fd = -1;
+    int load_attempt = 0;
+    unsigned int kernel_version = 0;
 
     prog_fd = bpf_object__load(obj);
+
+    // Load may fail if an incorrect kernel version number was passed to the
+    // bpf() syscall (old Linux kernels verify that, while newer kernels ignore it).
+    // Try a few methods of getting the kernel version and retry loading.
+    while (prog_fd < 0 && load_attempt < MAX_LOAD_METHODS)
+    {
+        kernel_version = get_kernel_version(load_attempt++);
+        if (kernel_version == 0)
+        {
+            continue;
+        }
+        bpf_object__set_kversion(obj, kernel_version);
+        prog_fd = bpf_object__load(obj);
+    }
+
     if (prog_fd < 0)
     {
         ebpf_log("failed to load BPF program\n");
