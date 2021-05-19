7085fdfa36ad9273dbe9c27998c1b4884633bfb6
Michal Stanek <michal.stanek@elastic.co>
2021-05-20 01:46:50 +0200

    Remove patch file for elftoolchain and do changes in git
    It is too cumbersome to use patch in build scripts so just track the changes in git.

--- contrib/build_libelf.sh
+++ contrib/build_libelf.sh
@@ -7,12 +7,9 @@
 # you may not use this file except in compliance with the Elastic License 2.0.
 
 
-set -u
-
-cat contrib/patch/elftoolchain.patch | patch -p0 -r - --forward -d contrib
+set -euv
 
 if [ ! -d contrib/elftoolchain/build ]; then
-    set -euv
     export MAKEFLAGS="" && export MFLAGS="" && \
         WITH_TESTS=no WITH_BUILD_TOOLS=no WITH_ADDITIONAL_DOCUMENTATION=no WITH_PE=no WITH_ISA=no   \
         bmake -C contrib/elftoolchain
