1af028f0ff83cdb30b16d0bf25328efbd13e93fd
Michal Stanek <michal.stanek@elastic.co>
2021-05-21 01:22:08 +0200

    Separate dir for libelf build objects

--- contrib/build_libelf.sh
+++ contrib/build_libelf.sh
@@ -10,11 +10,13 @@
 set -euv
 
 if [ ! -d contrib/elftoolchain/build ]; then
+    mkdir -p contrib/elftoolchain/build_obj
     export MAKEFLAGS="" && export MFLAGS="" && \
         WITH_TESTS=no WITH_BUILD_TOOLS=no WITH_ADDITIONAL_DOCUMENTATION=no WITH_PE=no WITH_ISA=no   \
+        MAKEOBJDIR=${PWD}/contrib/elftoolchain/build_obj \
         bmake -C contrib/elftoolchain
     mkdir -p contrib/elftoolchain/build
-    cp contrib/elftoolchain/libelf/libelf.a contrib/elftoolchain/build
+    cp contrib/elftoolchain/build_obj/libelf.a contrib/elftoolchain/build
     cp contrib/elftoolchain/libelf/libelf.h contrib/elftoolchain/build
     cp contrib/elftoolchain/libelf/gelf.h contrib/elftoolchain/build
     cp contrib/elftoolchain/common/elfdefinitions.h contrib/elftoolchain/build
