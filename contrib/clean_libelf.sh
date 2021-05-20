f8ad0948e615cda8152accfcf8772c4fa97927d8
Michal Stanek <michal.stanek@elastic.co>
2021-05-21 01:22:08 +0200

    Separate dir for libelf build objects

--- contrib/clean_libelf.sh
+++ contrib/clean_libelf.sh
@@ -11,5 +11,7 @@ set -euv
 
 export MAKEFLAGS="" && export MFLAGS="" && \
     WITH_TESTS=no WITH_BUILD_TOOLS=no WITH_ADDITIONAL_DOCUMENTATION=no WITH_PE=no WITH_ISA=no   \
+    MAKEOBJDIR=${PWD}/contrib/elftoolchain/build_obj \
     bmake -C contrib/elftoolchain clean
 rm -rf contrib/elftoolchain/build
+rm -rf contrib/elftoolchain/build_obj
