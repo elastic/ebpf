297bc49a41d44f5ab21e6dd31ba821bb7575ba6b
Michal Stanek <michal.stanek@elastic.co>
2021-05-20 21:49:49 +0200

    Fix issue with libbpf refusing to reload the program
    Apparently libbpf does not allow retrying to load a program. The object must be created and
    initialized from scratch, then loaded again. Rework the code to achieve that.
    Unfortunately now the caller must handle passing the load method and iterating over all methods.

--- non-GPL/HostIsolation/KprobeConnectHook/KprobeLoader.h
+++ non-GPL/HostIsolation/KprobeConnectHook/KprobeLoader.h
@@ -9,6 +9,14 @@
 
 #include <Common.h>
 
+enum ebpf_load_method
+{
+    EBPF_METHOD_NO_OVERRIDE = 0,
+    EBPF_METHOD_VDSO,
+    EBPF_METHOD_VERSION_H,
+    EBPF_MAX_LOAD_METHODS,
+};
+
 struct bpf_object *
 ebpf_open_object_file(const char *file_path);
 
@@ -18,7 +26,8 @@ ebpf_map_set_pin_path(struct bpf_object *obj,
                       const char *map_path);
 struct bpf_link *
 ebpf_load_and_attach_kprobe(struct bpf_object *obj,
-                            const char *program_sec_name);
+                            const char *program_sec_name,
+                            enum ebpf_load_method load_method);
 
 void
 ebpf_link_destroy(struct bpf_link *link);
