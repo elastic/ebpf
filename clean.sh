5c21444fc5bca779838301ef100b444f61e3db98
Michal Stanek <michal.stanek@elastic.co>
2021-05-25 00:04:52 +0200

    Correct SPDX identifier for Elastic License 2.0

--- clean.sh
+++ clean.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# SPDX-License-Identifier: LicenseRef-Elastic-License-2.0
+
+# Copyright 2021 Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+
+
 # run 'MAKESYSPATH=<dir> build.sh' to use custom share/mk directory for bmake
 
 set -euv
