04ae13769d3e227da10a19626e518cb6e2ecf44b
Michal Stanek <michal.stanek@elastic.co>
2021-05-25 00:04:52 +0200

    Correct SPDX identifier for Elastic License 2.0

--- contrib/build_libelf.sh
+++ contrib/build_libelf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# SPDX-License-Identifier: LicenseRef-Elastic-License-2.0
+
+# Copyright 2021 Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+
+
 set -u
 
 cat contrib/patch/elftoolchain.patch | patch -p0 -r - --forward -d contrib
