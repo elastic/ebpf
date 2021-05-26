096ea3db5ec936d6755451de7cda0675f27a1ac4
Michal Stanek <michal.stanek@elastic.co>
2021-05-27 01:57:06 +0200

    add MAKESYSPATH to bmake in clean_libelf.sh

--- contrib/clean_libelf.sh
+++ contrib/clean_libelf.sh
@@ -2,7 +2,7 @@
 
 set -euv
 
-export MAKEFLAGS="" && export MFLAGS="" && \
+export MAKEFLAGS="" && export MFLAGS="" && MAKESYSPATH=/opt/endpoint-dev/dev/toolchain/share/mk \
     WITH_TESTS=no WITH_BUILD_TOOLS=no WITH_ADDITIONAL_DOCUMENTATION=no WITH_PE=no WITH_ISA=no   \
     MAKEOBJDIR=${PWD}/contrib/elftoolchain/build_obj \
     bmake -C contrib/elftoolchain clean
