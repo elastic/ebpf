eb34417a47b9e121c212ecfc49654aa68b7849a6
Michal Stanek <michal.stanek@elastic.co>
2021-06-08 01:31:53 +0200

    Add doxygen comments in header files

--- non-GPL/HostIsolationMapsUtil/UpdateMaps.h
+++ non-GPL/HostIsolationMapsUtil/UpdateMaps.h
@@ -4,8 +4,20 @@
 // Host Isolation - tool for updating map of allowed IPs and pids
 //
 
+/**
+ * @brief Add a single IP to the IP allowlist
+ *
+ * @param[in] IPaddr IP address in uint format
+ * @return Error value (0 for success)
+ */
 int
 ebpf_map_allowed_IPs_add(uint32_t IPaddr);
 
+/**
+ * @brief Add a single PID (process ID) to the PID allowlist
+ *
+ * @param[in] pid PID number
+ * @return Error value (0 for success)
+ */
 int
-ebpf_map_allowed_pids_add();
+ebpf_map_allowed_pids_add(uint32_t pid);
--- non-GPL/TcLoader/TcLoader.h
+++ non-GPL/TcLoader/TcLoader.h
@@ -33,16 +33,43 @@ struct netlink_ctx {
     struct netlink_msg      msg;
 };
 
+/**
+ * @brief Add qdisc to a network interface
+ *
+ * @param[in] ifname Network interface name
+ * @return Error value (0 for success)
+ */
 int 
 netlink_qdisc_add(const char *ifname);
 
+/**
+ * @brief Remove qdisc from a network interface
+ *
+ * @param[in] ifname Network interface name
+ * @return Error value (0 for success)
+ */
 int
 netlink_qdisc_del(const char *ifname);
 
+/**
+ * @brief Add eBPF tc filter to a network interface (initialize only)
+ *
+ * @param[in] ctx Context containing netlink state - allocated and passed by caller
+ * @param[in] ifname Network interface name
+ * @return Error value (0 for success)
+ */
 int 
 netlink_filter_add_begin(struct netlink_ctx *ctx,
                          const char *ifname);
 
+/**
+ * @brief Add eBPF tc filter to a network interface (commit)
+ *
+ * @param[in] fd eBPF program file descriptor
+ * @param[in] ctx Context containing netlink state (from previous add_begin() call) - passed by caller
+ * @param[in] ebpf_obj_filename eBPF object filename
+ * @return Error value (0 for success)
+ */
 int 
 netlink_filter_add_end(int fd,
                        struct netlink_ctx *ctx,
