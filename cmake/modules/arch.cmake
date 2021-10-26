7c7868ac111d5e9a5c30ad857587e8b444eb3623
Lorenzo Fontana <lorenzo.fontana@elastic.co>
2021-10-26 19:20:27 +0200

    Allow targets to be used as a subdirectory

--- cmake/modules/arch.cmake
+++ cmake/modules/arch.cmake
@@ -4,5 +4,9 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 
-set(ARCH ${CMAKE_HOST_SYSTEM_PROCESSOR})
-string(REPLACE "x86_64" "x86" ARCH_TRUNC ${ARCH})
+if (NOT ARCH)
+    set(ARCH "${CMAKE_HOST_SYSTEM_PROCESSOR}")
+endif()
+if (NOT ARCH_TRUNC)
+    string(REPLACE "x86_64" "x86" ARCH_TRUNC ${ARCH})
+endif()
