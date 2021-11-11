d1573cc864378eca77ac8b41de62d1a8892fb75e
Leonardo Di Donato <leodidonato@gmail.com>
2021-11-11 18:28:51 +0100

    deps(contrib): bump to libbpf 0.5.0
    This commit bumps the libbpf dependency to its v0.5.
    
    On top of it, it adds the patches to make it work with elftoolchain.
    
    Signed-off-by: Leonardo Di Donato <leonardo.didonato@elastic.co>

--- contrib/libbpf/scripts/sync-kernel.sh
+++ contrib/libbpf/scripts/sync-kernel.sh
@@ -45,11 +45,14 @@ PATH_MAP=(									\
 	[tools/include/uapi/linux/if_link.h]=include/uapi/linux/if_link.h	\
 	[tools/include/uapi/linux/if_xdp.h]=include/uapi/linux/if_xdp.h		\
 	[tools/include/uapi/linux/netlink.h]=include/uapi/linux/netlink.h	\
+	[tools/include/uapi/linux/pkt_cls.h]=include/uapi/linux/pkt_cls.h	\
+	[tools/include/uapi/linux/pkt_sched.h]=include/uapi/linux/pkt_sched.h	\
+	[Documentation/bpf/libbpf]=docs						\
 )
 
 LIBBPF_PATHS="${!PATH_MAP[@]} :^tools/lib/bpf/Makefile :^tools/lib/bpf/Build :^tools/lib/bpf/.gitignore :^tools/include/tools/libc_compat.h"
 LIBBPF_VIEW_PATHS="${PATH_MAP[@]}"
-LIBBPF_VIEW_EXCLUDE_REGEX='^src/(Makefile|Build|test_libbpf\.c|bpf_helper_defs\.h|\.gitignore)$'
+LIBBPF_VIEW_EXCLUDE_REGEX='^src/(Makefile|Build|test_libbpf\.c|bpf_helper_defs\.h|\.gitignore)$|^docs/(\.gitignore|api\.rst|conf\.py)$|^docs/sphinx/.*'
 LINUX_VIEW_EXCLUDE_REGEX='^include/tools/libc_compat.h$'
 
 LIBBPF_TREE_FILTER="mkdir -p __libbpf/include/uapi/linux __libbpf/include/tools && "$'\\\n'
@@ -266,12 +269,12 @@ for patch in $(ls -1 ${TMP_DIR}/patches | tail -n +2); do
 done
 
 # Generate bpf_helper_defs.h and commit, if anything changed
-# restore Linux tip to use bpf_helpers_doc.py
+# restore Linux tip to use bpf_doc.py
 cd_to ${LINUX_REPO}
 git checkout ${TIP_TAG}
 # re-generate bpf_helper_defs.h
 cd_to ${LIBBPF_REPO}
-"${LINUX_ABS_DIR}/scripts/bpf_helpers_doc.py" --header			    \
+"${LINUX_ABS_DIR}/scripts/bpf_doc.py" --header					      \
 	--file include/uapi/linux/bpf.h > src/bpf_helper_defs.h
 # if anything changed, commit it
 helpers_changes=$(git status --porcelain src/bpf_helper_defs.h | wc -l)
