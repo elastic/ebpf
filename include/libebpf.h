67305fa635c9eb40a40e7eef4b9bf9e0b5117869
Alex Bell <tabell@users.noreply.github.com>
2021-11-16 18:47:29 +0000

    proposal: add context to maintain state, expose fd

--- include/libebpf.h
+++ include/libebpf.h
@@ -38,6 +38,10 @@ typedef enum ebpf_event_type {
     EVENT_NETWORK_IPV6_CONNECTION_ACCEPTED  = 9,
     EVENT_NETWORK_IPV6_RECONNECT_ATTEMPTED  = 10,
     EVENT_NETWORK_IPV6_HTTP_REQUEST         = 11,
+    EVENT_PROCESS_FORK,
+    EVENT_PROCESS_EXEC,
+    EVENT_PROCESS_EXIT,
+    EVENT_PROCESS_TTY_OUTPUT,
     EBPF_EVENT_MAX
 } ebpf_event_type;
 
@@ -47,9 +51,56 @@ struct ebpf_event {
     void *data;
 };
 
+/* opaque context, will contain union with struct perf_buffer,
+ * struct ring_buffer,
+ *
+ * we need a way to map from capability/event sets to probe sets,
+ * and we could store the cap/event sets directly or cache the result
+ * of the mapping in the context */
+typedef struct ebpf_event_ctx;
+
 typedef int (*event_handler_fn)(struct ebpf_event *event, size_t);
 
-void *ebpf_event__buffer_new(event_handler_fn handler);
-void ebpf_event__next(void *buffer);
+typedef uint32_t ebpf_cap_set;
+
+/* result will be ((ACTUAL AND INTERESTED) OR FORCED) */
+ebpf_cap_set ebpf_event__detect_capabilities(
+        ebpf_cap_set interested,
+        ebpf_cap_set forced)
+
+/* Combination of events user is interested in. Mapping of events
+ * to probe that provides that event is stored internally. User
+ * does not need to know which probes to load */
+typedef uint32_t ebpf_event_set;
+
+/* allocates a new context based on requested events and capabilities.
+ *
+ * returns a positive int that represents an fd, which
+ * can be used with epoll on success. returns -1 and
+ * sets errno on failure
+ */
+int ebpf_event__buffer_new(
+        ebpf_event_ctx **ctx,
+        ebpf_cap_set,
+        ebpf_event_set);
+
+/* this func will not return data unless fd is ready
+ * so user will supply their own epoll and call this
+ * func when fd is readable. I think passing a handler
+ * function is more versatile than passing a buffer?
+ * no strong feelings on that. The handler will likely
+ * dispatch one of another layer of callbacks switched
+ * on event type.
+ *
+ * We should also discuss blocking/nonblocking semantics
+ * here. I think this function could return -1 and set
+ * errno to -EAGAIN if the context is in a nonblocking
+ * state but no data is available. */
+int ebpf_event__next(
+        ebpf_event_ctx *ctx,
+        event_handler_fn *handler_func);
+
+void ebpf_event__cleanup(
+        ebpf_event_ctx *ctx);
 
 #endif // LIBEBPF_H_
