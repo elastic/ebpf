b4c8ab77c9250f99238fd8a6bd17e89dd15dcab7
Rhys Rustad-Elliott <rhys.rustad-elliott@elastic.co>
2022-01-11 23:58:09 +0000

    Disambiguate licensing to GPL 2.0 only
    There were two problems with the license headers as things stood:
    
    - The SPDX license identifiers used the deprecated "GPL-2.0" instead of
      the newer and less ambigous "GPL-2.0-only"
    - The longer GPL header comments all stated that the files were licensed
      under GPL v3 or later, while the SPDX identifier above said GPL 2.0
    
    This commit changes all the SPDX "GPL-2.0" headers to "GPL-2.0-only" and
    replaces the longer GPL header comments with ones that explicitly state
    version 2 only (see https://spdx.org/licenses/GPL-2.0-only.html).

--- GPL/EventProbe/PathResolver.h
+++ GPL/EventProbe/PathResolver.h
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 /*
+ * Copyright (C) 2021 Elasticsearch BV
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/*
  * BPF dentry resolver
  *
  * This file contains code needed to construct a path as a string from Linux's
