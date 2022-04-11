9fd64ab4a136a6af5f6f0925c09a1bbe0c8f311b
Rhys Rustad-Elliott <rhys.rustad-elliott@elastic.co>
2022-04-11 17:24:11 +0000

    Fix improper memset
    Size argument was always 8. We can't use memset since we don't know the
    len of buf, but buf[0] = '\0' works just as well.
    
    Co-Authored-By: Nicholas Berlin <nicholas.berlin@elastic.co>

--- GPL/EventProbe/PathResolver.h
+++ GPL/EventProbe/PathResolver.h
@@ -76,9 +76,9 @@ ebpf_resolve_path_to_string(char *buf, struct path *path, const struct task_stru
     struct dentry *curr_dentry = BPF_CORE_READ(path, dentry);
     struct dentry **dentry_arr;
 
-    // Ensure we zero out buf early up here so if we exit with any sort of
-    // error, we won't leave garbage in it if it's uninitialized
-    memset(buf, '\0', sizeof(buf));
+    // Ensure we make buf and empty string early up here so if we exit with any
+    // sort of error, we won't leave garbage in it if it's uninitialized
+    buf[0] = '\0';
 
     u32 zero = 0;
     if (!(dentry_arr = bpf_map_lookup_elem(&path_resolver_scratch_map, &zero))) {
