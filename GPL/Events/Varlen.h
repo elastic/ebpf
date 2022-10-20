994a5c3b615da83b2408f15f61f14e7ae7713f8e
Rhys Rustad-Elliott <rhys.rustad-elliott@elastic.co>
2022-10-20 23:14:48 +0000

    Remove BUFFER_SIZE_HALF logic
    Given the low caps on potentially large varlen params (argv, tty_out,
    etc.), this bounding is unnecessary. It was back when we were reading
    EVENT_BUFFER_SIZE_HALF as the maximum length of a new variable length
    param, but doesn't do us anything now.

--- GPL/Events/Varlen.h
+++ GPL/Events/Varlen.h
@@ -23,15 +23,10 @@
 // size). bpf_ringbuf_reserve/bpf_ringbuf_submit should be used instead to
 // avoid the extra memory copy for better performance.
 
-// 256 KiB per cpu core, 128 KiB is useable as we bail if we go over half
-//
-// Currently, the largest possible event (with variable length parameters all
-// of their respective maximum lengths) is well under 128 KiB. This limit
-// should be bumped if this is no longer the case or else the verifier will
-// start to complain.
+// 256 KiB per cpu core, this is currently more than large enough to handle the
+// largest theoretical event, but should be bumped in the future if that
+// changes or else the verifier will start to complain.
 #define EVENT_BUFFER_SIZE (1 << 18)
-#define EVENT_BUFFER_SIZE_HALF (EVENT_BUFFER_SIZE >> 1)
-#define EVENT_BUFFER_SIZE_HALF_MASK (EVENT_BUFFER_SIZE_HALF - 1)
 
 // Convenience macro to determine the current size of an event with its
 // variable length fields
@@ -68,9 +63,8 @@ static void *get_event_buffer()
 struct ebpf_varlen_field *ebpf_vl_field__add(struct ebpf_varlen_fields_start *fields,
                                              enum ebpf_varlen_field_type type)
 {
-    struct ebpf_varlen_field *new_field =
-        (struct ebpf_varlen_field *)(&fields->data[fields->size & EVENT_BUFFER_SIZE_HALF_MASK]);
-    new_field->type = type;
+    struct ebpf_varlen_field *new_field = (struct ebpf_varlen_field *)(&fields->data[fields->size]);
+    new_field->type                     = type;
     fields->nfields++;
     return new_field;
 }
