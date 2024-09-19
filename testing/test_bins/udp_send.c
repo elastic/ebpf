802fa43304abfc9b9f4238ccf7e2bdd19b7fc182
fearful-symmetry <alexpkristiansen@icloud.com>
2024-09-19 07:35:02 -0700

    format

--- testing/test_bins/udp_send.c
+++ testing/test_bins/udp_send.c
@@ -1,48 +1,42 @@
-#define _GNU_SOURCE 		/* program_invocation_name */
-#include <sys/types.h>
+#define _GNU_SOURCE /* program_invocation_name */
 #include <sys/socket.h>
+#include <sys/types.h>
 
 #include <netinet/in.h>
 
 #include <arpa/inet.h>
 
-#include <unistd.h>
+#include <err.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <err.h>
+#include <unistd.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	struct sockaddr_in	sin;
-	int			sock, ch, do_connect;
-	ssize_t			n;
-    char* inaddr = "127.0.0.1";
-	uint64_t		buf[] = {
-		0xdeadbeef, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef,
-		0xdeadbeef, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef
-	};
+    struct sockaddr_in sin;
+    int sock, ch, do_connect;
+    ssize_t n;
+    char *inaddr   = "127.0.0.1";
+    uint64_t buf[] = {0xdeadbeef, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef,
+                      0xdeadbeef, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef};
 
-	bzero(&sin, sizeof(sin));
+    bzero(&sin, sizeof(sin));
 
+    if (inet_aton(inaddr, &sin.sin_addr) == 0)
+        errx(1, "inet_aton(%s)", inaddr);
+    sin.sin_port   = htons(53);
+    sin.sin_family = AF_INET;
 
-	if (inet_aton(inaddr, &sin.sin_addr) == 0)
-		errx(1, "inet_aton(%s)", inaddr);
-	sin.sin_port = htons(53);
-	sin.sin_family = AF_INET;
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+        err(1, "socket");
 
-	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-		err(1, "socket");
+    n = sendto(sock, buf, sizeof(buf), 0, (struct sockaddr *)&sin, sizeof(sin));
+    if (n == -1)
+        err(1, "sendto");
+    else if (n != sizeof(buf))
+        errx(1, "sendto: shortcount");
 
-
-	n = sendto(sock, buf, sizeof(buf), 0,
-	    (struct sockaddr *)&sin, sizeof(sin));
-	if (n == -1)
-		err(1, "sendto");
-	else if (n != sizeof(buf))
-		errx(1, "sendto: shortcount");
-
-	return (0);
+    return (0);
 }
