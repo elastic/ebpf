b6ac2366686cb123cb50e74a676a8f450ec2229e
fearful-symmetry <alexpkristiansen@icloud.com>
2024-09-20 13:08:33 -0700

    try different compile args?

--- testing/scripts/gen_initramfs.sh
+++ testing/scripts/gen_initramfs.sh
@@ -37,7 +37,7 @@ build_testrunner() {
     pushd testrunner > /dev/null
 
     go clean
-    GOARCH=$goarch go test -c
+    GOARCH=$goarch CGO_ENABLED=0 go test -c -o testrunner -ldflags '-extldflags "-static"'
 
     if [[ $? -ne 0 ]]
     then
@@ -78,7 +78,7 @@ invoke_bluebox() {
 
     local cmd="bluebox"
     cmd+=" -a $goarch"
-    cmd+=" -e testrunner/testrunner.test"
+    cmd+=" -e testrunner/testrunner"
     cmd+=" -r $eventstrace"
     cmd+=" -r $tcfiltertests"
     cmd+=" -r $tcfilterbpf"
