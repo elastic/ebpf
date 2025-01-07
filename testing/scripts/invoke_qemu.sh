fcce0f951c8c382086e864b41e5ac612c13df24c
fearful-symmetry <alexpkristiansen@icloud.com>
2025-01-07 13:14:26 -0800

    change machine type

--- testing/scripts/invoke_qemu.sh
+++ testing/scripts/invoke_qemu.sh
@@ -96,7 +96,7 @@ main() {
     elif [[ $arch == "x86_64" ]]; then
         # x86_64 uses ttyS0 for the first serial port
         bootparams+=" console=ttyS0"
-        extra_args+=" -machine virt"
+        extra_args+=" -machine kvm"
     fi
 
     qemu-system-${arch} \
