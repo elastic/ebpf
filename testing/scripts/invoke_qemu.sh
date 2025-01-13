2475471f12bbdffd2d26c1bca4c11edfb6be1b05
fearful-symmetry <alexpkristiansen@icloud.com>
2025-01-13 19:46:52 +0000

    tinkering

--- testing/scripts/invoke_qemu.sh
+++ testing/scripts/invoke_qemu.sh
@@ -96,7 +96,7 @@ main() {
     elif [[ $arch == "x86_64" ]]; then
         # x86_64 uses ttyS0 for the first serial port
         bootparams+=" console=ttyS0"
-        extra_args+=" -machine accel=tcg"
+        extra_args+=" -machine accel=kvm"
     fi
 
     qemu-system-${arch} \
